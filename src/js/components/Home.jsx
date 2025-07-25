960c20c462ce014f3a0489d5aae28675e6f4221d
Angel-enc <enock8876@gmail.com>
2025-07-25 18:30:49 +0000

    ultima version que funciona flata boton limpiar funcion delete y no se guardan las tareas

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/index.css";
 //import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 
 const Home = () => {
     const [tarea, setTarea]= useState([]); // Estado para la lista de tareas
     const [nuevaTarea, setNuevaTarea] = useState(""); // Estado para manejar lo que el usuario escribe
-
+    const [usuarioCreado, setUsuarioCreado] = useState(false); 
     // Función para manejar el "Enter" en el input
     const handleKeyUp = (e) => {
         if (e.key === "Enter" && nuevaTarea.trim() !== "") {
@@ -21,6 +22,67 @@ const Home = () => {
         setTarea(actualizaTareas);
     };
 
+    const crearTarea= async () => {
+        const response = await fetch("https://playground.4geeks.com/todo/todos/Angel", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ label: nuevaTarea, done: false })
+        });
+        const data = response.json();
+        console.log(data);
+        setNuevaTarea(""); 
+        getTareas(); 
+    }
+    const crearUsuario = async () => {
+        try{
+            const response = await fetch("https://playground.4geeks.com/todo/users/Angel", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify([])
+            });
+            if (!response.ok) {
+                console.log("Error al crear el usuario");
+                setUsuarioCreado(true);
+                return;
+            }
+            console.log(response);
+            const data = await response.json()
+            console.log(data);
+            setUsuarioCreado(true);
+            getTareas();
+        }catch (error) {
+            console.log("Error al crear el usuario", error);
+            setUsuarioCreado(true);
+            }
+        };
+
+    const getTareas = async () => {
+        try {
+        const response = await fetch("https://playground.4geeks.com/todo/users/Angel");
+        console.log(response);
+        if (!response.ok) {
+            console.log("No existe el usuario, crear usuario");
+            if (!usuarioCreado) await crearUsuario();
+            return;
+            }
+            const data = await response.json()
+            console.log(data);
+            setTarea(data.tareas || []); //Para asegurarme que data.tareas existe y es un array.
+        } catch (error) {
+            console.log("Error al obtener las tareas", error);
+            if (!usuarioCreado) await crearUsuario();
+        }
+        
+                
+    }
+    useEffect(() => {
+        getTareas();
+    }, []);
+
     return (
         <div className="container text-center mt-5 todo-container">
             <h1 className="title">Lista de tareas.</h1>
@@ -32,7 +94,9 @@ const Home = () => {
                 onChange={(e) => setNuevaTarea(e.target.value)}
                 id="tarea"
                 name="tarea"
-                onKeyUp={handleKeyUp}
+                onKeyUp={(e) =>{
+                    if (e.key === "Enter") crearTarea();
+                }}
             />
             <ul className="list-group shadow">
                 {tarea.length === 0 ? (
